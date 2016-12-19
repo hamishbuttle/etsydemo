cab4da7dffa711bf644721a9257bc9733ca0b6e4
hamish buttle <hammishh@gmail.com>
2016-12-19 12:59:44 +1100

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ActiveRecord::Base
 
+ belongs_to :user
+
   has_attached_file :image, styles: { medium: "200x", thumb: "100x100" }, default_url: "default.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
@@ -7,5 +9,5 @@ class Listing < ActiveRecord::Base
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
 
-  validates :image
+  validates :imag, presence: true
 end
